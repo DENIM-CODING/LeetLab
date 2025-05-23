478e50fb35c2adfa043bfc697811fbceaf14af40
DENIM <denim@DENIMs-MacBook-Air.local>
2025-05-23 16:36:40 +0530

    Register controller implemented

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+
 import authRoutes from "./routes/auth.routes.js";
 
 dotenv.config()
@@ -7,12 +10,13 @@ dotenv.config()
 const app = express();
 
 app.use(express.json())
+app.use(cookieParser())
 
 app.get('/', (req,res) => {
     res.send("Welcome to LeetLab!");
 })
 
-app.use("api/v1/auth",authRoutes);
+app.use("/api/v1/auth",authRoutes);
 
 app.listen(process.env.PORT,()=>{
     console.log(`Server is runnning on Port: 8080`);
--- backend/src/libs/db.js
+++ backend/src/libs/db.js
@@ -1,7 +1,7 @@
-import {PrismaCliet} from "../generated/prisma/index.js"
+import {PrismaClient} from "../generated/prisma/index.js"
 
 const globalForPrisma = globalThis;
 
-export const db = globalForPrisma || new PrismaCliet();
+export const db = globalForPrisma.prisma || new PrismaClient();
 
-if(process.env.NODE_ENV != "production") globalForPrisma = db
+if(process.env.NODE_ENV != "production") globalForPrisma.prisma = db
