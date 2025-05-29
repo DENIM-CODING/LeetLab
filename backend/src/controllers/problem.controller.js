9ff623c6292f2d409a11271bbe3c11df398f6c78
DENIM <denim@DENIMs-MacBook-Air.local>
2025-05-29 17:01:21 +0530

    create problem controller and judge0.lib.js created

--- backend/src/controllers/problem.controller.js
+++ backend/src/controllers/problem.controller.js
@@ -1,4 +1,100 @@
+import {db} from "../libs/db.js"
+import { getJudge0LanguageId, submitBatch } from "../libs/judge0.lib.js";
+
+
 export const createProblem = async (req, res) => {
+
+    const {
+    title,
+    description,
+    difficulty,
+    tags,
+    examples,
+    constraints,
+    testcases,
+    codeSnippets,
+    referenceSolutions,
+    } = req.body;
+
+    if(req.user.role !== "ADMIN"){
+        return res.status(403).json({error: "You are not allowed to create a problem"})
+    }
+
+    try {
+        
+        for (const [language, solutionCode] of Object.entries(referenceSolutions)) {
+        const languageId = getJudge0LanguageId(language); // utility to make code readable
+
+        if (!languageId) {
+            return res
+            .status(400)
+            .json({ error: `Language ${language} is not supported` });
+        }
+
+        //preparing judge0 submission for all the test cases
+
+        const submissions = testcases.map(({ input, output }) => ({
+            source_code: solutionCode,
+            language_id: languageId,
+            stdin: input,
+            expected_output: output,
+         })); // created array of submission for each language
+
+         //would accept submissions
+         const submissionResults = await submitBatch(submissions); // token obatined
+
+         const tokens = submissionResults.map((res) => res.token); // token extracted
+
+         //polling
+
+         const results = await pollBatchResults(tokens);
+
+         for(let i = 0; i < results.length; i++){
+            const result = results[i];
+            if (result.status.id !== 3) {
+                return res.status(400).json({
+                    error: `Testcase ${i + 1} failed for language ${language}`,
+                });
+            }
+         }
+
+         //save the problem to the database
+
+         const newProblem = await db.problem.create({
+            data: {
+                title,
+                description,
+                difficulty,
+                tags,
+                examples,
+                constraints,
+                testcases,
+                codeSnippets,
+                referenceSolutions,
+                userId: req.user.id,
+            },
+        });
+
+        return res.status(201).json({
+            sucess: true,
+            message: "Message Created Successfully",
+            problem: newProblem,
+        });
+         
+
+
+
+
+
+    }
+
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            error: "Error While Creating Problem",
+        });
+    }
 
 }
 
---
+++ backend/src/libs/judge0.lib.js
@@ -0,0 +1,51 @@
+export const getJudge0LanguageId = (language) => {
+     const languageMap = {
+        "PYTHON":71,
+        "JAVA":62,
+        "JAVASCRIPT":63,
+    }
+
+    //if we want to make it support for more languages then we have to include that here first
+
+    return languageMap[language.toUpperCase()]
+}
+
+
+//would hit the endpoint of judge0 (refer to judge0 docs in submission/create a batch section)
+//when we hit the url first time it would give me token in an array
+export const submitBatch = async (submissions)=>{
+    const {data} = await axios.post(`${process.env.JUDGE0_API_URL}/submissions/batch?base64_encoded=false`,{
+        submissions
+    })
+
+
+    console.log("Submission Results: ", data)
+
+    return data // [{token} , {token} , {token}] token corresponding to the resp code in submission
+}
+
+
+const sleep  = (ms)=> new Promise((resolve)=> setTimeout(resolve , ms))
+
+
+//to poll the endpoint whether our work is done or not
+export const pollBatchResults = async (tokens)=>{
+    while(true){
+        
+        const {data} = await axios.get(`${process.env.JUDGE0_API_URL}/submissions/batch`,{
+            params:{
+                tokens:tokens.join(","),
+                base64_encoded:false,
+            }
+        })
+
+        const results = data.submissions;
+
+        const isAllDone = results.every(
+            (r)=> r.status.id !== 1 && r.status.id !== 2
+        )
+
+        if(isAllDone) return results
+        await sleep(1000)
+    }
+}
