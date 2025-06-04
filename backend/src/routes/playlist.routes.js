c5590c47cde0ef27f7d110249f02685f1198b543
DENIM <khannadenim@gmail.com>
2025-06-04 17:52:04 +0530

    playlist route and controller implemented

---
+++ backend/src/routes/playlist.routes.js
@@ -0,0 +1,17 @@
+import express from "express";
+import { authMiddleware } from "../middleware/auth.middleware.js";
+import { addProblemToPlaylist, createPlayList, deletePlayList, getAllPlayListDetails, getPlayListDetails, removeProblemFromPlaylist } from "../controllers/playlist.controller.js";
+
+const playlistRoutes = express.Router();
+
+playlistRoutes.get("/" , authMiddleware , getAllPlayListDetails)
+playlistRoutes.get("/:playlistId" , authMiddleware , getPlayListDetails)
+
+playlistRoutes.post("/create-playlist" ,authMiddleware ,  createPlayList)
+playlistRoutes.post('/:playlistId/add-problem' , authMiddleware , addProblemToPlaylist)
+
+playlistRoutes.delete("/:playlistId" , authMiddleware , deletePlayList)
+playlistRoutes.delete("/:playlistId/remove-problem" , authMiddleware , removeProblemFromPlaylist)
+
+
+export default playlistRoutes;
