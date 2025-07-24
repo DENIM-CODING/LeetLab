72deecd620331d5f281448fc5aa5a4b6c095bc32
DENIM <khannadenim@gmail.com>
2025-07-25 04:22:39 +0530

    auth from ui done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import HomePage from './page/HomePage.jsx'
 import SignUpPage from './page/SignUpPage.jsx'
 import { Loader } from 'lucide-react'
 import { useAuthStore } from './store/useAuthStore.js'
+import Layout from './layout/Layout.jsx'
 
 
 const App = () => {
@@ -29,10 +30,14 @@ const App = () => {
     <div className='flex flex-col items-center justify-start'>
       <Toaster/>
       <Routes>
-        <Route
-        path='/'
-        element={authUser ? <HomePage /> : <Navigate to={"/login"} />}
-        />
+
+        <Route path="/" element={<Layout />}>
+          <Route
+            index
+            element={authUser ? <HomePage /> : <Navigate to={"/login"} />}
+          />
+        </Route>
+
         <Route
           path="/login"
           element={!authUser ? <LoginPage /> : <Navigate to={"/"} />}
