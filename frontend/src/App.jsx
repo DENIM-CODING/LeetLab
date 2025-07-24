e1e40e482f81cb9525b39a9c84e3859669525bdb
DENIM <khannadenim@gmail.com>
2025-07-24 16:03:06 +0530

    routing fo home login signup page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,30 @@
 import React from 'react'
-import { Route, Navigator, Routes} from "react-router-dom"
+import { Route, Navigate, Routes} from "react-router-dom"
 import LoginPage from './page/LoginPage.jsx'
 import HomePage from './page/HomePage.jsx'
 import SignUpPage from './page/SignUpPage.jsx'
 
 const App = () => {
+
+  let authUser = null;
+
   return (
     <div className='flex flex-col items-center justify-start'>
       <Routes>
         <Route
         path='/'
-        element={<HomePage/>}
+        element={authUser ? <HomePage /> : <Navigate to={"/login"} />}
         />
         <Route
-        path='/login'
-        element={<LoginPage/>}
+          path="/login"
+          element={!authUser ? <LoginPage /> : <Navigate to={"/"} />}
         />
+
         <Route
-        path='/signup'
-        element={<SignUpPage/>}
+          path="/signup"
+          element={!authUser ? <SignUpPage /> : <Navigate to={"/"} />}
         />
+        
       </Routes>
 
     </div>
