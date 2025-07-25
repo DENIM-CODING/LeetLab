ad244785e5a1f2ba327fc752d9e4cf075d3a8e73
DENIM <khannadenim@gmail.com>
2025-07-25 16:41:25 +0530

    CreateProblemForm created

--- frontend/src/page/AddProblem.jsx
+++ frontend/src/page/AddProblem.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import CreateProblemForm from '../components/CreateProblemForm'
 
 const AddProblem = () => {
   return (
     <div>
-      AddProblem
+      <CreateProblemForm/>
     </div>
   )
 }
