cce6ba103cc6c2d6d1f08099c5a34926fcbfba44
DENIM <khannadenim@gmail.com>
2025-07-26 13:30:19 +0530

    section to show problem added in homepage

---
+++ frontend/src/components/ProblemTable.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ProblemTable = ({problems}) => {
+  return (
+    <div>
+     {
+        JSON.stringify(problems)
+     }
+    </div>
+  )
+}
+
+export default ProblemTable
